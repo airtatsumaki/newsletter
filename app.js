f4e6e49769579b706fc6085100755d95577c3d5c
tatsu <naz1mb20@gmail.com>
2022-09-16 11:02:07 +0100

    updated app js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
