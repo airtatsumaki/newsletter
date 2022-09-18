44bcf1904c69566d2a1a09fa2db46f320081c1ff
tatsu <naz1mb20@gmail.com>
2022-09-18 14:56:55 +0100

    updated paths with public static folder

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req,res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.listen(3000, () => console.log("Server is running!"));
